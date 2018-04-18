6e50c248a390c3028db9dfeedef18552355adf97
emory93 <38482558+emory93@users.noreply.github.com>
2018-04-17 22:09:07 -0500

    Create Find smallest number in array.java

---
+++ Find smallest number in array.java
@@ -0,0 +1,8 @@
+import java.util.stream.IntStream;
+
+
+public class SmallestIntegerFinder {
+    public static int findSmallestInt(int[] args) {
+return IntStream.of(args).min().getAsInt();
+    }
+}
